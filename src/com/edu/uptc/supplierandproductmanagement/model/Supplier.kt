849862925771fb840489886f5ab24d3f0296436e
LReyesA29 <luis.reyes4029@gmail.com>
2025-11-06 14:06:59 -0500

    fnl

--- src/com/edu/uptc/supplierandproductmanagement/model/Supplier.kt
+++ src/com/edu/uptc/supplierandproductmanagement/model/Supplier.kt
@@ -1,11 +1,11 @@
 package com.edu.uptc.supplierandproductmanagement.model
 
 data class Supplier(
-    val code: String,      // unique code, e.g.: "SUP001"
-    var name: String,      // supplier name
-    var city: String,      // city
-    var address: String,   // address
-    var phone: String,     // phone number
-    var email: String,     // email
-    var deliveryTime: Int  // average delivery time in days
+    val code: String,
+    var name: String,
+    var city: String,
+    var address: String,
+    var phone: String,
+    var email: String,
+    var deliveryTime: Int
 )
